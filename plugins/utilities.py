d58f263a82cb136bbf46ca00583689652ff953e3
Gregory Johnson <gregorytj@outlook.com>
2018-02-13 01:01:20 -0700

    added scope to /plugin list

--- plugins/utilities.py
+++ plugins/utilities.py
@@ -2,7 +2,7 @@
 from telegram import ChatAction as Ca
 from telegram.ext import CommandHandler, RegexHandler, MessageHandler
 
-from helpers import clean
+from helpers import clean, is_mod
 
 handlers = []
 
@@ -23,9 +23,33 @@ handlers.append([CommandHandler('start', start), {'action': Ca.TYPING, 'name': T
 
 def list_plugins(bot, update, bot_globals):
     """lists plugins and their commands"""
+
+    message = update.message
+    user = message.from_user
+    chat = message.chat
+    message_user = user.username if user.username is not None else user.name
+
+    if chat.type == 'private':
+        admins_list = [message_user]
+    else:
+        admins_list = [x.user.username for x in bot.getChatAdministrators(chat_id=message.chat_id,
+                                                                          message_id=message.message_id)]
+
+    def scope(p):
+        if p.handlers[1]:
+            if 'mods' in p.handlers[1].keys() and p.handlers[1]['mods'] and not is_mod(message_user):
+                return False
+
+            admin = message_user in admins_list or is_mod(message_user)
+
+            if 'admins' in p.handlers[1].keys() and p.handlers[1]['admins'] and not admin:
+                return False
+
+        return True
+
     text = ''
     name = clean(update.message.text)
-    plugins = bot_globals['PLUGINS']
+    plugins = {k: p for k, p in bot_globals['PLUGINS'].items() if scope(p)}
 
     if name in plugins:
         p = plugins[name]
