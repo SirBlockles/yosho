5a166f18c81a642e107682212a1ded96db585627
Gregory Johnson <gregorytj@outlook.com>
2018-02-13 17:28:48 -0700

    /markov generator modification

--- plugins/markov.py
+++ plugins/markov.py
@@ -143,6 +143,11 @@ def markov(bot, update):
             branches = branches[1:]
             probabilities = probabilities[1:]
 
+        # don't repeat the same thing twice in a row
+        for i, b in enumerate(branches):
+            if b == state_index and STATES[b]:
+                probabilities[i] = 0
+
         # normalization of probabilities
         transition_sum = sum(probabilities)
         probabilities = tuple(i/transition_sum for i in probabilities)
@@ -348,7 +353,6 @@ def insert(bot, update):
     """insert new state in markov states"""
     expr = clean(update.message.text)
     state = expr.split()[0]
-    state = state
 
     accumulator(bot, update, insert=state)
 
